4f5b7a9964e75cd1cc34a7e62b766b573197a6ef
ci247 <ci247@cam.ac.uk>
2022-01-30 17:14:00 +0000

    finalised task1B demonstration program

--- Task1B.py
+++ Task1B.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
+from turtle import distance
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 from haversine import haversine, Unit
@@ -12,16 +13,16 @@ def run():
 
     # Build list of stations
     stations = build_station_list()
+    
+    stations_by_distance_list = stations_by_distance(stations, (52.2053, 0.1218))
+    i = 0
+    answer = []
 
-    # Print number of stations
-    print("Number of stations: {}".format(len(stations)))
+    for station,distance in stations_by_distance_list:
+        answer += [(station.name, station.town, distance)]
+    print("The 10 closest stations from Cambridge city centre ", answer[:10])
+    print("The 10 furthest stations from Cambridge city centre ", answer[-10:])
 
-    # Display data from 3 stations:
-    for station in stations:
-        if station.name in [
-                'Bourton Dickler', 'Surfleet Sluice', 'Gaw Bridge'
-        ]:
-            print(station)
-
-print("The 10 closest entries (in km) are ", stations_by_distance(build_station_list(), (52.2053, 0.1218))[:10])
-print("The 10 furthest entries (in km) are ", stations_by_distance(build_station_list(), (52.2053, 0.1218))[-10:])
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
