9cf5ce60e19c8e869d0d0df8bcf5ff86e9a8b41e
heramb-modugula <hrm66@cam.ac.uk>
2022-01-30 11:05:51 +0000

    added func for Task 1D

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def rivers_with_station(stations):
+    """Build and return a list of all rivers with river level monitoring stations
+    based on data provided to the fucniton. The expected input is a list of MonitoringStation
+    objects.
+    """
+
+    rivers = set()
+    
+    for station in stations:
+        if station.river != None:
+            rivers.append(Station.river)
+    
+    return rivers
