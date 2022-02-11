6b47216f591a18929f6595eb4f2b93f429e2db65
ci247 <ci247@cam.ac.uk>
2022-02-11 22:27:57 +0000

    task2B demonstration program complete

---
+++ Task2B.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_level_over_threshold
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    #store list of tuples: stations_level_over_threshold(stations, 0.8)
+    slot = stations_level_over_threshold(stations, tol = 0.8)
+
+    
+    for station, slot in slot:
+        print(station.name, slot)
+
+
+if __name__ == "__main__":
+    print("*** Task 2A: CUED Part IA Flood Warning System ***")
+    run()
