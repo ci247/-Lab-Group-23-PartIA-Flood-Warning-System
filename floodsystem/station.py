18ef535f495d761ded92e50dcf12ab2e48dbc8ca
ci247 <ci247@cam.ac.uk>
2022-02-11 18:28:41 +0000

    updated relative_water_level() for discussion

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -51,6 +51,10 @@ class MonitoringStation:
         else:
             return True
 
+    def relative_water_levels(self):
+
+        d = (((self.latest_level - self.typical_range[0]) / (self.typical_range[1] - self.typical_range[0])))
+        return d
 
 def inconsistent_typical_range_stations(stations):
     """Build and return a list of all stations with inconsistent typical ranges based
