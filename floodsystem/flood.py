deb00509b19323210bc961f4f171e42ab04c6ebb
ci247 <ci247@cam.ac.uk>
2022-02-12 00:13:20 +0000

    task2C built stations_highest_rel_level function in submodule flood

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -16,3 +16,11 @@ def stations_level_over_threshold(stations, tol):
             pass
     ans.sort(key = lambda a: a[1], reverse = True)
     return ans
+
+def stations_highest_rel_level(stations, N):
+    #stores all [(station objects, relative water levels)] as list of tuples in descending order
+    all_descending = stations_level_over_threshold(stations, -9999)
+    ans = []
+    for i in range(N):
+        ans += [all_descending[i][0]]
+    return ans
